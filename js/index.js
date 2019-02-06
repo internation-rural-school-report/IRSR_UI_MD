b2a8c3a99f7ccff29750b94c7726f22f42c99225
Md Kawsar Hussen <mdkawsarhussen@Mds-MacBook-Pro.local>
2019-02-06 11:46:45 -0500

    Body content updated, need to link item with Navigations, Working on Js File for that

--- js/index.js
+++ js/index.js
@@ -1,23 +1,54 @@
+
 class TabLink {
-    constructor(tabElement){
-      this.tabElement = tabElement;
-      this.tabData = this.tabElement.dataset.tab;
-      this.cards = Array.from(this.cards).map(card => new TabCard(card));
-      this.tabElement.addEventListener("click", () => this.selectTab()); 
+  constructor(element) {
+    // Assign this.element to the passed in DOM element
+    this.element= element; 
+    // Get the custom data attribute on the Link
+    // this.data;
+    this.data = this.element.dataset.tab;
+    
+    // Using the custom data attribute get the associated Item element
+    // this.itemElement;
+    this.item = document.querySelector(`.cards-container[data-tab="${this.data}"]`);   
+    // Using the Item element, create a new instance of the TabItem class
+    // this.tabItem;
+    this.tabItem = new TabItem(this.item);
+    // Add a click event listener on this instance, calling the select method on click
+    this.element.addEventListener('click', () => {this.select()});
+  };
+
+  select() {
+    // Get all of the elements with the tabs-link class
+    // const links;
+    const links = document.querySelectorAll('.tab');
+    // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
+    // Array.from(links).forEach();
+     links.forEach(item => item.classList.remove('selected-tab'));
+    // Add a class named "tabs-link-selected" to this link
+    // this.element;
+    this.element.classList.add('selected-tab');
+    // Call the select method on the item associated with this link
+    this.tabItem.select();
   }
-    selectTab(){
-      const cards = document.querySelectorAll(".card");
-      cards.forEach(card => card.style.display = "none");
-      this.tabElement.classList.add(".tab");
-      this.cards.forEach(card => card.selectCard());
-    }
+}
+
+class TabItem {
+  constructor(element) {
+    // Assign this.element to the passed in element
+    // this.element;
+    this.element = element;
   }
-  class TabCard {
-    constructor(cardElement){
-      this.cardElement = cardElement;
+
+  select() {
+    // Select all ".tabs-item" elements from the DOM
+    // const items;
+    const items = document.querySelectorAll('.card');
+    // Remove the class "tabs-item-selected" from each element
+    items.forEach(item => item.classList.remove('selected-card'));
+    // Add a class named "tabs-item-selected" to this element
+    //this.element;
+    this.element.classList.add('selected-card');
     }
-    selectCard(){
-      this.cardElement.style.display = ".card";
-    }
-  }
-  let tabs = document.querySelectorAll(".tab").forEach(tab => new TabLink(tab));
+}
+
+let links = document.querySelectorAll('.card').forEach(link => new TabLink(link)); 
